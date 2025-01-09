9cedcd81d14e79ca401699d0a8c1850d1d488279
ch0el <157335212+ch0el@users.noreply.github.com>
2025-01-09 15:38:13 -0500

    Update hb-monitor23-24.py

--- hb-monitor23-24.py
+++ hb-monitor23-24.py
@@ -1,38 +1,32 @@
 # Heartbeat Sensor #1
 from adafruit_circuitplayground.express import cpx
 import time
-movement_threshold = 5
-sample_interval = 0.1
-trigger = False
-heartbeat = 0
+
+breath_count = 0
 start_time = time.monotonic()
 
 while True:
-    x_float, y_float, z_float = cpx.acceleration
+    z_acceleration = cpx.acceleration[2]
+    
     total_acceleration = abs(x_float) + abs(y_float) + abs(z_float)
-    if total_acceleration > movement_threshold:
-        heartbeat += 1
+    
+    if z_acceleration > 9.85:
+        breath_count += 1
+        
     current_time = time.monotonic()
     if current_time - start_time >= 10:
-        heartbeat = 0
-        average_heartbeat = heartbeat * 6
-        heartbeat = 0
-        current_time = start_time
-        if average_heartbeat > 205:
-            print("Baby's heart rate is too high")
-            print(average_heartbeat)
-            trigger = True
-        elif average_heartbeat < 100:
-            print("Baby's heart rate is too low")
-            print(average_heartbeat)
-            trigger = True
+        average_bpm = (breath_count / 10) * 60
+        print("Average breath per minute:", average_bpm)
+        if average_bpm > 60:
+            print("Respiratory rate is too high")
+            cpx.pixels.fill((255,0,0))
+        elif average_bpm < 30:
+            print("Respiratory rate is too low")
+            cpx.pixels.fill((255,255,0))
         else:
             print("Heartrate is normal.")
-        if trigger:
-            # put code for playing sound with speaker input
-            # put code for lighting up less (ideally blinking)
-            print("Average Heartbeat per minute:", average_heartbeat)
-    time.sleep(5)
+            cpx.pixels.fill((0,255,0))
+    time.sleep(0.1)
 
 
 
