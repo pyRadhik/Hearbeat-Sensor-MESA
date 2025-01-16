a80b4166686156d1ec5ecd9d478666f29e84bf30
ch0el <157335212+ch0el@users.noreply.github.com>
2025-01-16 15:28:40 -0500

    Update hb-monitor23-24.py

--- hb-monitor23-24.py
+++ hb-monitor23-24.py
@@ -5,15 +5,35 @@ import time
 breath_count = 0
 start_time = time.monotonic()
 
+SP = 9.8
+PV = float()
+dx = float()
+integral = float()
+derivative = float()
+error = float()
+
 while True:
-    z_acceleration = cpx.acceleration[2]
-    
-    total_acceleration = abs(x_float) + abs(y_float) + abs(z_float)
-    
-    if z_acceleration > 9.85:
-        breath_count += 1
-        
+    PV = cpx.acceleration[2]
     current_time = time.monotonic()
+    
+    
+    integral = (current_time - start_time) / 2 * (dx + SP - PV)
+    derivative = (SP - PV - dx) / (current_time - start_time)
+    
+    dx = SP - PV
+    
+    error = integral + derivative + dx;
+    
+    print((error,))
+    #print((PV,))
+    
+    
+
+    if PV > 9.85:
+        breath_count += 1
+
+    
+    
     if current_time - start_time >= 10:
         average_bpm = (breath_count / 10) * 60
         print("Average breath per minute:", average_bpm)
@@ -26,6 +46,11 @@ while True:
         else:
             print("Heartrate is normal.")
             cpx.pixels.fill((0,255,0))
+            
+        breath_count = 0
+        start_time = time.monotonic()
+        
+        
     time.sleep(0.1)
 
 
