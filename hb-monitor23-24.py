e193bdb4801364c7717bf63532d10a0723949a1b
nathanli5722 <152818045+nathanli5722@users.noreply.github.com>
2025-02-05 09:23:32 -0500

    Update hb-monitor23-24.py
    Update Heart Sensor w/ filter and HID. 
    TODO: Testing and Checking for errors

--- hb-monitor23-24.py
+++ hb-monitor23-24.py
@@ -1,56 +1,59 @@
 # Heartbeat Sensor #1
 from adafruit_circuitplayground.express import cpx
+
+import usb_hid
+from adafruit_hid.keyboard import Keyboard
+from adafruit_hid.keyboard_layout_us import KeyboardLayoutUS
+
 import time
 
-breath_count = 0
-start_time = time.monotonic()
 
-SP = 9.8
-PV = float()
-dx = float()
-integral = float()
-derivative = float()
-error = float()
+breath_count = 0
+start_time = prev_time = time.monotonic()
+acceleration = float();
+
+kbd = Keyboard(usb_hid.devices, timeout=10)
+layout = KeyboardLayoutUS(kbd)
+
+#will probably have errors, im editing in a txt file rn
+filter = Filter.RadialFilter(.025, .5, 0.75, 1)
 
 while True:
-    PV = cpx.acceleration[2]
+    acceleration = cpx.acceleration[2] - 9.81
     current_time = time.monotonic()
-    
-    
-    integral = (current_time - start_time) / 2 * (dx + SP - PV)
-    derivative = (SP - PV - dx) / (current_time - start_time)
-    
-    dx = SP - PV
-    
-    error = integral + derivative + dx;
-    
-    print((error,))
-    #print((PV,))
-    
-    
 
-    if PV > 9.85:
+    #todo: filter acceleration
+    acceleration = filter.filter(acceleration, current_time - prev_time)
+    
+    print((acceration,))
+    
+    if acceleration > .05:
         breath_count += 1
-
     
     
     if current_time - start_time >= 10:
         average_bpm = (breath_count / 10) * 60
         print("Average breath per minute:", average_bpm)
+
+        #try to send bpm to pc as a keyboard
+        layout.write(str(average_bpm) + " ", delay=.05)
+        
         if average_bpm > 60:
             print("Respiratory rate is too high")
-            cpx.pixels.fill((255,0,0))
+            cpx.pixels.fill((50,0,0))
+            
         elif average_bpm < 30:
             print("Respiratory rate is too low")
-            cpx.pixels.fill((255,255,0))
+            cpx.pixels.fill((33, 33,0))
+            
         else:
             print("Heartrate is normal.")
-            cpx.pixels.fill((0,255,0))
+            cpx.pixels.fill((0,50,0))
             
         breath_count = 0
         start_time = time.monotonic()
         
-        
+    prev_time = current_time
     time.sleep(0.1)
 
 
